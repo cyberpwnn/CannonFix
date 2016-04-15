bb28c04647a3e8c944eb5adeb89c5f1b101ccbc8
Daniel Mills <danielmillst@gmail.com>
2016-04-15 18:50:30 -0400

    Initial commit

---
+++ src/com/cyberpwn/cannons/CannonFix.java
@@ -0,0 +1,16 @@
+package com.cyberpwn.cannons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CannonFix extends JavaPlugin
+{
+	public void onEnable()
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+}
